a7ace31bb16085e5810af3e5e66e061105756615
quangnc <quangnc@mainjs.com>
2019-02-13 00:42:17 +0700

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+function App() {
+    const [count, setCount] = useState(0);
+    const [message, setMessage] = useState("");
+  
+    function increment() {
+      setCount(count + 1);
+    }
+  
+    function decrement() {
+      setCount(count - 1);
+    }
+  
+    useEffect(() => setMessage(`count is ${count}`), [count, message]);
+  
+    return (
+      <div className="App">
+        <h1>Update Count!</h1>
+        <p>Count: {count}</p>
+        <p>{message}</p>
+        <button type="button" onClick={increment}>
+          +
+        </button>
+        <button type="button" onClick={decrement}>
+          -
+        </button>
+      </div>
+    );
+  }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
